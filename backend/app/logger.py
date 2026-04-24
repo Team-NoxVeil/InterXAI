72892746d9a1e7bfe006d4df290b1915212f1010
Sathwik Shetty <sathwikshetty2k5@gmail.com>
2026-04-24 05:18:16 +0000

    backend/logger: Initalize logger.

---
+++ backend/app/logger.py
@@ -0,0 +1,21 @@
+import logging
+import sys
+
+from app.config import settings
+
+
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter(
+            fmt="[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+        logger.setLevel(logging.DEBUG if settings.DEBUG else logging.INFO)
+
+    return logger
