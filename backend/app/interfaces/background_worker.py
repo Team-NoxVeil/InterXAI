a39b0dd795c82aabd7c81a93f19034028db4880d
Sathwik Shetty <sathwikshetty2k5@gmail.com>
2026-05-12 09:38:27 +0530

    Revert "backend/worker: Extend the interface to include startup and shutdown."
    This reverts commit 03c7df310095c09a50e738022f421a0a9dc4d654.

--- backend/app/interfaces/background_worker.py
+++ backend/app/interfaces/background_worker.py
@@ -3,15 +3,7 @@ from abc import ABC, abstractmethod
 
 class BackgroundWorkerInterface(ABC):
     @abstractmethod
-    async def startup(self) -> None:
-        pass
-
-    @abstractmethod
-    async def shutdown(self) -> None:
-        pass
-
-    @abstractmethod
-    async def process_resume_task(  # type: ignore[misc]
+    async def process_resume_task(
         file_bytes_b64: str, file_name: str, application_id: int
     ) -> None:
         pass
